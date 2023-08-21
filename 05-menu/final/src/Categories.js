f77d4c016fc2055e3f44509b608d7fa6328fc0ec
Lợi <loivantran10012002@gmail.com>
2023-08-21 11:07:14 +0700

    loi done 05

---
+++ 05-menu/final/src/Categories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Categories = ({ filter, categories }) => {
+    return (
+        <div className="btn-container">
+            {categories.map((category, index) => {
+                return (
+                    <button
+                        className="filter-btn"
+                        key={index}
+                        onClick={() => filter(category)}
+                    >
+                        {category}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Categories;
